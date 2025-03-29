678840cbff7c08e1ae5dbf46b731fb3b9d1ac393
ankitydv12 <ankitydv9863@gmail.com>
2025-03-30 01:48:04 +0530

    rkjw

---
+++ Editor/Learn/JFileChooserDemo.java
@@ -0,0 +1,17 @@
+package Learn;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
+public class JFileChooserDemo {
+    public static void main(String[] args) {
+        JFileChooser fc = new JFileChooser();
+        int res = fc.showOpenDialog(null);
+        //fc.showOpenDialog return 0 if file choosed and if 1 if cancel to choose
+        /**
+         * JFileChooser.APPROVE_OPTION → If the user selects a file and clicks "Open" (APPROVE_OPTION=1)
+         *   JFileChooser.CANCEL_OPTION → If the user clicks "Cancel" (APPROVE_OPTION=0)
+         */
+
+    }
+}
--- Editor/editor.java
+++ Editor/editor.java
@@ -1,11 +1,16 @@
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.plaf.metal.MetalLookAndFeel;
@@ -16,12 +21,14 @@ public class editor extends JFrame implements ActionListener{
     //Creating a Jtext
     JTextArea txt;
     JFrame fm;
+
+     JButton mc;
     public editor()
     {
         fm = new JFrame();
-        fm.setVisible(true);
+      
          try {
-             UIManager.setLookAndFeel("java.swing.plt.nimbus.NimbusLookAndFeel");
+             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
              MetalLookAndFeel.setCurrentTheme(new OceanTheme());
 
          } catch (Exception e) {
@@ -29,55 +36,78 @@ public class editor extends JFrame implements ActionListener{
          }
 
         txt = new JTextArea();
+        txt.setFont(new Font("Arial", Font.BOLD, 25));
+        txt.setPreferredSize(new Dimension(150,40));
         JMenuBar mb = new JMenuBar();
+        mb.setFont(new Font("Arial", Font.BOLD, 25));
+        mb.setPreferredSize(new Dimension(150,40));
 
         JMenu m1 =  new JMenu("File");
+        m1.setFont(new Font("Arial", Font.BOLD, 25));
 
-        JMenuItem mi1 = new JMenuItem("New");
-        JMenuItem mi2 = new JMenuItem("Open");
-        JMenuItem mi3 = new JMenuItem("Save");
-        JMenuItem mi4 = new JMenuItem("Print");
+        JMenuItem[] file_iteaamsItem = {
+        new JMenuItem("New"),
+ 
+        new JMenuItem("Open"),
+        
+        new JMenuItem("Save"),
+        
+        new JMenuItem("Print")
+        };
 
-        mi1.addActionListener(this);
-        mi2.addActionListener(this);
-        mi3.addActionListener(this);
-        mi4.addActionListener(this);
+    for(JMenuItem x:file_iteaamsItem)
+    {
+        x.setFont(new Font("Arial", Font.BOLD, 25));
+        x.setPreferredSize(new Dimension(150,40));
+        m1.add(x);
+        x.addActionListener(this);
+    }
+    JMenu m2 =  new JMenu("Edit");
+    m2.setFont(new Font("Arial", Font.BOLD, 25));
+    JMenuItem[] edit_iteaamsItem = {
+        new JMenuItem("Cut"),
+ 
+        new JMenuItem("Copy"),
+        
+        new JMenuItem("Past"),
+        
+        };
 
-        m1.add(mi1);
-        m1.add(mi2);
-        m1.add(mi3);
-        m1.add(mi4);
+    for(JMenuItem x:edit_iteaamsItem)
+    {
+        x.setFont(new Font("Arial", Font.BOLD, 25));
+        x.setPreferredSize(new Dimension(150,40));
+        m2.add(x);
+        x.addActionListener(this);
+    }
 
-        JMenu m2 = new JMenu("Edit");
-        JMenuItem m4 = new JMenuItem("cut");
-        JMenuItem m5 = new JMenuItem("copy");
-        JMenuItem m6 = new JMenuItem("past");
-
-        m2.add(m4);
-        m2.add(m5);
-        m2.add(m6);
-
-        JMenuItem mc = new JMenuItem("close");
+        mc = new JButton("close");
+        mc.setFont(new Font("Arial", Font.BOLD, 25));
+        mc.addActionListener(this);
+       
 
         mb.add(m1);
         mb.add(m2);
         mb.add(mc);
-
-        fm.setMenuBar(mb);
-        fm.add(mc);
-        fm.add(txt);
-        fm.setDefaultCloseOperation(EXIT_ON_CLOSE);
         
 
-
-
+        fm.setJMenuBar(mb);
+        fm.setSize(1200,800);
+        fm.setResizable(true);
+        fm.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        fm.setVisible(true);
+        fm.setLocationRelativeTo(null);
+        fm.add(txt);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
         
         
+        if(e.getSource()==mc)
+            System.exit(ABORT);
+        
     }
     public static void main(String[] args) {
-        editor obj = new editor();
+       new editor();
     }
 }
