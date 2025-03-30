4d9e62caa255d51ea1ea7bee0efa7fc96de08d77
ankitydv12 <ankitydv9863@gmail.com>
2025-03-30 11:36:48 +0530

    JfileChooser

--- Editor/Learn/JFileChooserDemo.java
+++ Editor/Learn/JFileChooserDemo.java
@@ -6,7 +6,10 @@ import javax.swing.filechooser.FileSystemView;
 public class JFileChooserDemo {
     public static void main(String[] args) {
         JFileChooser fc = new JFileChooser();
-        int res = fc.showOpenDialog(null);
+        /*
+         * JFileChooser() – empty constructor that points to user’s default directory  
+         */
+        fc.showOpenDialog(null);
         //fc.showOpenDialog return 0 if file choosed and if 1 if cancel to choose
         /**
          * JFileChooser.APPROVE_OPTION → If the user selects a file and clicks "Open" (APPROVE_OPTION=1)
